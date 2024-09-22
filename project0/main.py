16dd1761e7dcc0afa9ff11c6497c842a5a51fc85
prathamrao021 <prathamrao021@gmail.com>
2024-09-22 15:30:57 -0400

    divided the data in rows and column

--- project0/main.py
+++ project0/main.py
@@ -1,4 +1,6 @@
 import urllib.request
+from pypdf import PdfReader
+import pandas as pd
 
 def fetchincidents(url):
     headers = {}
@@ -7,5 +9,23 @@ def fetchincidents(url):
     data = urllib.request.urlopen(urllib.request.Request(url, headers=headers)).read()
     print(data)
 
+def extractincidents(pdf):
+    page = PdfReader(pdf).pages[0]
+    # print(page.extract_text(layout_mode='layout', layout_mode_scale_weight=2.0))
+    data = page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False)
+    
+    print(data)
+    lines = (data.split("\n"))[3:]
+    
+    records = [line.split() for line in lines]
+    
+    df = pd.DataFrame(records)
+    # print(df)
+    
+    
+    
+    
+
 if __name__ == "__main__":
-    fetchincidents("https://www.normanok.gov/sites/default/files/documents/2024-08/2024-08-02_daily_arrest_summary.pdf")
+    # fetchincidents("https://www.normanok.gov/sites/default/files/documents/2024-08/2024-08-02_daily_arrest_summary.pdf")
+    extractincidents("docs/Incidetnt1.pdf")
